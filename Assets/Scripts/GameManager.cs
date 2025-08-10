1aad172f37bbae4f60fe23932eb71fc94359de5c
maradw <maria.mendoza@tecsup.edu.pe>
2025-08-09 21:32:23 -0500

    movement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,8 @@
 
+using DG.Tweening;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -13,27 +16,45 @@ public class GameManager : PersistentSingleton<GameManager>
     public static event Action OnGameStart;
     public static event Action OnGamePaused;
     public static event Action OnGameResumed;
+    [SerializeField] GameObject coins;
 
-   // public static event Action OnGameOver;
+    // public static event Action OnGameOver;
+   [SerializeField] Transform objectToMove;
+    [SerializeField] List<Transform> sequence;
 
+    [SerializeField] Transform sidePlatform;
 
     public static event Action OnWin;
     private void OnEnable()
     {
         StartGame();
+        PlayerControl.OnCollisionActivateSide += MoveSide;
+        PlayerControl.OnCollisionActivateFall += FallPlatform;
+        PlayerControl.OnCollisionActiveteSequence += StartSequence;
     }
     private void OnDisable()
     {
-        
-
-        
-
+        PlayerControl.OnCollisionActivateSide -= MoveSide;
+        PlayerControl.OnCollisionActivateFall -= FallPlatform;
+        PlayerControl.OnCollisionActiveteSequence -= StartSequence;
     }
-    
+   void MoveSide()
+    {
+        sidePlatform.DOMoveX(sidePlatform.position.x + 20f, 1f)
+            .SetEase(Ease.InOutQuad)
+            .OnComplete(() => Debug.Log("Platform moved successfully!"));
+    }
+
+    void FallPlatform()
+    {
+        objectToMove.DOMoveY(objectToMove.position.y - 20f, 1f)
+            .SetEase(Ease.InOutQuad)
+            .OnComplete(() => Debug.Log("Platform fell successfully!"));
+    }
     public void StartGame()
     {
-        AudioManager.Instance.StopSound();
-        AudioManager.Instance.PlayMusicIndex(1);  
+        //AudioManager.Instance.StopSound();
+        //AudioManager.Instance.PlayMusicIndex(1);  
         isGameRunning = true;
         isPaused = false;
         Time.timeScale = 1f; 
@@ -83,6 +104,22 @@ public class GameManager : PersistentSingleton<GameManager>
     public void InitializeGameSession()
     {
         OnGameStart?.Invoke();
+    }
+    IEnumerator SequenceFall()
+    {
+        for (int i = 0; i < sequence.Count; i++)
+        {
+            Transform currentObject = sequence[i];
+            currentObject.DOMoveY(currentObject.position.y - 20f, 1f)
+                .SetEase(Ease.InOutQuad)
+                .OnComplete(() => Debug.Log("Platform " + i + " fell successfully!"));
+            yield return new WaitForSeconds(0.6f);
+        }
+        //yield return new WaitForSeconds (2f);
+    }
+    void StartSequence()
+    {
+        StartCoroutine(SequenceFall());
     }
     /*
     private void OnCollisionEnter(Collision collision)
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+public class PlayerControl : MonoBehaviour
+{
+    private float _horizontal;
+    [SerializeField] private Rigidbody2D myRBD;
+
+    [SerializeField] private float velocityModifier;
+
+    [SerializeField] private float jumpForce;
+
+    [SerializeField] private int maxJumps = 2;
+    [SerializeField] private int gravity;
+    [SerializeField] private int jumpCount = 0;
+    int life = 10;
+
+    public static event Action<int> OnCollisionItem;
+
+    public static event Action OnCollisionActiveteSequence;
+    public static event Action OnCollisionActivateFall;
+    public static event Action OnCollisionActivateSide;
+    void Start()
+    {
+
+    }
+
+    public void OnMovement(InputAction.CallbackContext move)
+    {
+        _horizontal = move.ReadValue<Vector2>().x;
+    }
+    public void OnJump(InputAction.CallbackContext jump)
+    {
+        if (jump.performed && jumpCount < maxJumps)
+        {
+            myRBD.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            
+            jumpCount += 1;
+            //myRBD.linearVelocityY
+        }
+        if (jump.canceled)
+        {
+            isInTheAir = true;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Ground")
+        {
+
+            jumpCount = 0;
+            isInTheAir = false;
+
+        }
+
+        if (collision.gameObject.tag == "Coin")
+        {
+            OnCollisionItem?.Invoke(5);
+            Destroy(collision.gameObject);
+            Debug.Log("Coin");
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ActiveSide")
+        { 
+            collision.gameObject.SetActive(false);
+            Debug.Log("activate side");
+            OnCollisionActivateSide?.Invoke();
+        }
+        if (collision.gameObject.tag == "ActiveFall")
+        {
+            collision.gameObject.SetActive(false);
+            Debug.Log("activate fall");
+            OnCollisionActivateFall?.Invoke();
+        }
+        if (collision.gameObject.tag == "ActiveSequence")
+        {
+            collision.gameObject.SetActive(false);
+            Debug.Log("activate sequence");
+            OnCollisionActiveteSequence?.Invoke();
+        }
+
+    }
+    void Update()
+    {
+        // Debug.Log(jumpCount);
+    }
+    [SerializeField] bool isInTheAir;
+    public void FixedUpdate()
+    {
+        myRBD.linearVelocity = new Vector2(_horizontal * velocityModifier, myRBD.linearVelocity.y);
+
+        if(isInTheAir == true)
+        {
+            myRBD.gravityScale = gravity;
+        }
+        else
+        {
+            myRBD.gravityScale = 1f;
+        }
+    }
+}
